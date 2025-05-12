c263b7e2a76c3eb7a796c317bca80a12499fc4f3
Jaeyeong Won <devlog.jae13@gmail.com>
2025-05-12 23:02:31 +0900

    Add files via upload

---
+++ HW04/Assets/HW04_2176225_WJY/HW04_WJY_Item_Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HW04_WJY_Item_Controller : MonoBehaviour
+{
+    public GameObject PickController;
+    private void OnMouseDown()
+    {
+        PrintInfo();
+        PickController.GetComponent<HW04_WJY_Pick_Controller>().Add_Click(gameObject);
+    }
+
+    void PrintInfo()
+    {
+        print($"{gameObject.name}을/를 클릭했습니다.");
+    }
+}
